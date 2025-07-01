f5ab1c1980f0d90c88396f2847ce975df56f1db7
ankitkumarsharma <aksharma.dixit@gmail.com>
2025-07-02 01:38:43 +0530

    blogs updated dynamic

--- src/app/components/blog/blog.ts
+++ src/app/components/blog/blog.ts
@@ -10,9 +10,7 @@ import { BLOGS } from '../../core/constants/blogs.constant';
 export class Blog {
   readonly blogs = BLOGS;
   tabActiveIndex:number = 0;
-  getDataTabId(index: number) {
-    return `tab${index + 1}`
-  }
+  
   onTabFn(index: number) {
     this.tabActiveIndex = index;
   }
--- src/app/core/constants/blogs.constant.ts
+++ src/app/core/constants/blogs.constant.ts
@@ -1,63 +1,69 @@
 export const BLOGS = {
   tabMenu: ['Blogs', 'Talks'],
-  tab1:[
+  tabList: [
     {
-      category:'Development',
-      link:'https://medium.com/@ankit_k_sharma/component-interaction-in-angular-438d6d50430c',
-      src:'../assets/img/blog/blog1.png',
-      title:'Component Interaction in Angular',
-      date:'15 Apr 2022',
+      tab: [
+        {
+          category: 'Development',
+          link: 'https://medium.com/@ankit_k_sharma/component-interaction-in-angular-438d6d50430c',
+          src: '../assets/img/blog/blog1.png',
+          title: 'Component Interaction in Angular',
+          date: '15 Apr 2022',
+        },
+        {
+          category: 'Development',
+          link: 'https://medium.com/@ankit_k_sharma/how-to-use-multiple-environment-variables-in-angular-7c9dcb485cd7',
+          src: 'https://miro.medium.com/v2/resize:fit:1100/format:webp/1*-LD4KHAh4NU3tubEAicLFQ.jpeg',
+          title: 'How to use multiple Environment Variables in Angular',
+          date: '7 May 2022',
+        },
+        {
+          category: 'Development',
+          link: 'https://medium.com/@ankit_k_sharma/angular-standalone-component-future-of-angular-95d1c8fd8d0d',
+          src: 'https://miro.medium.com/v2/resize:fit:1100/format:webp/1*hJTs6OzSAJaVYgWD7TEYnw.png',
+          title: 'Angular Standalone Component — Future of Angular',
+          date: '08 Jul 2022',
+        },
+        {
+          category: 'Development',
+          link: 'https://medium.com/@ankit_k_sharma/hoisting-in-java-script-859279d3b2ec',
+          src: 'https://miro.medium.com/v2/resize:fit:1100/format:webp/1*oBZDBCylMK3silXlrJesPw.jpeg',
+          title: 'Hoisting in Java Script',
+          date: '10 Jul 2022',
+        },
+        {
+          category: 'Development',
+          link: 'https://blog.stackademic.com/how-to-implement-ngrx-store-in-angular-apps-1129c7360c8c',
+          src: 'https://miro.medium.com/v2/resize:fit:1100/format:webp/1*iOC2HaKvG4mb7LwbdxvrFg.png',
+          title: 'How To Implement NGRX Store In Angular Apps',
+          date: '21 Sep 2023',
+        },
+        {
+          category: 'Development',
+          link: 'https://blog.stackademic.com/whats-new-in-angular-17-592b7d157c6f',
+          src: 'https://miro.medium.com/v2/resize:fit:1100/format:webp/1*PEgVd9jkSLzgDx5ygBUbag.png',
+          title: 'What’s New in Angular 17',
+          date: '21 Nov 2023',
+        }
+      ]
     },
     {
-      category:'Development',
-      link:'https://medium.com/@ankit_k_sharma/how-to-use-multiple-environment-variables-in-angular-7c9dcb485cd7',
-      src:'https://miro.medium.com/v2/resize:fit:1100/format:webp/1*-LD4KHAh4NU3tubEAicLFQ.jpeg',
-      title:'How to use multiple Environment Variables in Angular',
-      date:'7 May 2022',
-    },
-    {
-      category:'Development',
-      link:'https://medium.com/@ankit_k_sharma/angular-standalone-component-future-of-angular-95d1c8fd8d0d',
-      src:'https://miro.medium.com/v2/resize:fit:1100/format:webp/1*hJTs6OzSAJaVYgWD7TEYnw.png',
-      title:'Angular Standalone Component — Future of Angular',
-      date:'08 Jul 2022',
-    },
-    {
-      category:'Development',
-      link:'https://medium.com/@ankit_k_sharma/hoisting-in-java-script-859279d3b2ec',
-      src:'https://miro.medium.com/v2/resize:fit:1100/format:webp/1*oBZDBCylMK3silXlrJesPw.jpeg',
-      title:'Hoisting in Java Script',
-      date:'10 Jul 2022',
-    },
-    {
-      category:'Development',
-      link:'https://blog.stackademic.com/how-to-implement-ngrx-store-in-angular-apps-1129c7360c8c',
-      src:'https://miro.medium.com/v2/resize:fit:1100/format:webp/1*iOC2HaKvG4mb7LwbdxvrFg.png',
-      title:'How To Implement NGRX Store In Angular Apps',
-      date:'21 Sep 2023',
-    },
-    {
-      category:'Development',
-      link:'https://blog.stackademic.com/whats-new-in-angular-17-592b7d157c6f',
-      src:'https://miro.medium.com/v2/resize:fit:1100/format:webp/1*PEgVd9jkSLzgDx5ygBUbag.png',
-      title:'What’s New in Angular 17',
-      date:'21 Nov 2023',
+      tab: [
+        {
+          category: 'Talks',
+          link: 'https://medium.com/@ankit_k_sharma/conducted-a-free-webinar-on-angular-essentials-for-corporate-folks-63fa4d60daa0',
+          src: 'https://miro.medium.com/v2/resize:fit:1100/format:webp/1*G-ZqB_ST_J6ewwJaRH6yfQ.png',
+          title: 'Conducted a free webinar on Angular Essentials for Corporate Folks',
+          date: '14 May 2022',
+        },
+        {
+          category: 'Talks',
+          link: 'https://blog.stackademic.com/code-once-use-everywhere-the-power-of-angular-libraries-f86d2edf89de',
+          src: 'https://miro.medium.com/v2/resize:fit:1100/format:webp/1*AkKRzHFynzNEEMkzwoHp9Q.jpeg',
+          title: 'DevFest: Code Once, Use Everywhere: The Power of Angular Libraries',
+          date: '11 Nov 2024',
+        },
+      ]
     }
-  ],
-  tab2:[
-    {
-      category:'Talks',
-      link:'https://medium.com/@ankit_k_sharma/conducted-a-free-webinar-on-angular-essentials-for-corporate-folks-63fa4d60daa0',
-      src:'https://miro.medium.com/v2/resize:fit:1100/format:webp/1*G-ZqB_ST_J6ewwJaRH6yfQ.png',
-      title:'Conducted a free webinar on Angular Essentials for Corporate Folks',
-      date:'14 May 2022',
-    },
-    {
-      category:'Talks',
-      link:'https://blog.stackademic.com/code-once-use-everywhere-the-power-of-angular-libraries-f86d2edf89de',
-      src:'https://miro.medium.com/v2/resize:fit:1100/format:webp/1*AkKRzHFynzNEEMkzwoHp9Q.jpeg',
-      title:'DevFest: Code Once, Use Everywhere: The Power of Angular Libraries',
-      date:'11 Nov 2024',
-    },
   ]
 }
